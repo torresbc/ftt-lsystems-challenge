5f4313ad34a81bf75f211aac37df1e648aa5b789
Bruna Torres <bcristorres@gmail.com>
2020-11-03 00:41:23 -0300

    Create def export_svg

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ftt-lsystems-challenge
+Faculdade de Tecnologia Termomecanica
+Bruna Cristina Torres - 081180005 - EC06
+
+alphabet: B, R, U
+beginning: BRU
+rules: (B -> RU), (R -> BU), (U -> R)
+        - B -> Draw the biggest star
+        - R -> Draw the smallest star
+        - U -> Move forward 5
+        
+       
--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-"""
-alphabet: B, R, U
-beginning: BRU
-rules: (B -> RU), (R -> BU), (U -> R)
-        - B -> Draw the biggest star
-        - R -> Draw the smallest star
-        - U -> Move forward 5
-"""
 import turtle
+import math
 
 
 def read_txt():
@@ -25,23 +18,29 @@ def read_txt():
     return rules, beginning
 
 
-def draw_star(beginning):
+def draw_star(beginning, angle, actual_point):
     """
     Draw the stars
     :param beginning: Current compiled phrase
     """
+    points = ""
     for letter in beginning:
         for i in range(5):
             if letter == "B":
-                turtle.forward(120)
-                turtle.left(216)
-                turtle.end_fill()
+                actual_point = [actual_point[0] + (120 * math.cos(math.radians(angle))), actual_point[1] +
+                                (120 * math.sin(math.radians(angle)))]
+                angle -= 216
             if letter == "R":
-                turtle.forward(60)
-                turtle.left(216)
-                turtle.end_fill()
+                actual_point = [actual_point[0] + (60 * math.cos(math.radians(angle))), actual_point[1] +
+                                (60 * math.sin(math.radians(angle)))]
+                angle -= 216
             if letter == "U":
-                turtle.forward(5)
+                actual_point = [actual_point[0] + (5 * math.cos(math.radians(angle))), actual_point[1] +
+                                (5 * math.sin(math.radians(angle)))]
+            points += f'{actual_point[0]}, {actual_point[1]} '
+            print(points)
+
+    return points
 
 
 def replace_beginning(rules, beginning):
@@ -64,20 +63,39 @@ def replace_beginning(rules, beginning):
     return final_beginning
 
 
+def export_svg(points):
+    """
+    Export the output_txt string into a SVG file
+    :param points: Draw list of coordinates
+    """
+    size = 310
+    output_txt = f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
+      "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+      <svg height="{size}" width="{size}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve">
+      <polyline points="'''
+
+    output_txt += f'{points}"  style="fill:white;stroke:rgb(255,0,0);stroke-width:3" /></svg>'
+
+    output = open("output.svg", "w")
+    output.write(output_txt)
+    output.close()
+
+
 def main():
     rules, beginning = read_txt()
+    angle = 216
+    actual_point = [285, 285]
+    points = ""
 
     for i in range(5):
-        draw_star(beginning)
+        points += draw_star(beginning, angle, actual_point) + ","
         beginning = replace_beginning(rules, beginning)
 
-    turtle.done()
+    export_svg(points)
 
 
 if __name__ == '__main__':
-    star = turtle.Turtle()
-    star.getscreen().bgcolor("gray")
-    star.speed(10)
     main()
 
 
